85b87f4f69a6c84d31c4c598f85b478432e6df3e
JerryJHC <jerryhenriquez@gmail.com>
2016-12-08 11:49:32 +0100

    Add layout. Add recognize vocal and gesture raw file.

--- app/src/main/java/com/example/hc_pc/vocalesgestos/MainActivity.java
+++ app/src/main/java/com/example/hc_pc/vocalesgestos/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.hc_pc.vocalesgestos;
 
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GestureOverlayView.OnGesturePerformedListener {
+
+    private GestureOverlayView gestureView;
+    private EditText nombreGesto;
+    private GestureLibrary gestLib;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gestureView = (GestureOverlayView) findViewById(R.id.gestures);
+
+
+        nombreGesto = (EditText) findViewById(R.id.nombreGesto);
+
+        gestLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if (!gestLib.load()) {
+            Toast.makeText(this, "Error al cargar gestos predefinidos", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gestureView.addOnGesturePerformedListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        gestureView.removeOnGesturePerformedListener(this);
+        super.onStop();
+    }
+
+
+    @Override
+    public void onGesturePerformed(GestureOverlayView gOverlayView, Gesture gesture) {
+        ArrayList<Prediction> predicciones = gestLib.recognize(gesture);
+
+        // We want at least one prediction
+        if (predicciones.size() > 0) {
+            Prediction prediccion = predicciones.get(0);
+            // We want at least some confidence in the result
+            if (prediccion.score > 1.0) {
+                String texto = nombreGesto.getText().toString() + prediccion.name;
+                nombreGesto.setText(texto);
+            }
+        }
     }
 }
